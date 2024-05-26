9ece6762569132e9ed7619b6c9bf3c1012f3315f
abdo261 <abdellah.bs2018@gmail.com>
2024-05-26 18:51:05 +0100

    template electrone

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "abdellah_app",
+    width: 1000,
+    height: 600,
+  });
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, "index.html"),
+    protocol: "file",
+  });
+  mainWindow.loadURL(startUrl)
+}
+app.whenReady().then(createMainWindow)
